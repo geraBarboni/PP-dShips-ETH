857d6aa32603db0c270098b805afdde41c056ab8
Gera Barboni <63870012+geraBarboni@users.noreply.github.com>
2023-12-05 11:52:33 -0300

    fixing .env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: { NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
